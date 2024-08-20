013bf0e22abf4b4686d6e974571bdfabbcb9eed6
Rishabh Suny <rishabhsunyriz@gmail.com>
2024-08-20 06:24:39 +0000

    added some script

---
+++ JS-game/assets/js/script.js
@@ -0,0 +1,47 @@
+const wolverine = document.getElementsByClassName(demo)
+const typeMessage = document.getElementsByClassName(right)
+const nextMessage = document.getElementsByClassName(left)
+const
+
+function  while (condition) {
+    
+}
+
+
+function updateGame() {}
+
+function (element,forEach(img => {}
+    imgResponsive = left.value
+    imgEnlarge = func(getElementsByClassName). $``;
+);
+
+function { createLeft(demo => demo.split()) 
+
+}
+
+function createGame(min , max) {
+    Math.floor(Math.random()* 5+1)
+
+   if (Math.floor(Math.random())* 5 !)* 10
+    indexof(createGame(0))
+
+    else
+    Math.floor(Math.random())* 5 !)* 10 > 10
+    return constrndInt = false
+}
+
+
+function readScore() {
+    let rndInt = createGame(1 , 1000)
+    rndInt.forEach(constrndInt => {
+        updateGame().toggle = ? 1, 0
+        p.demo.createLeft = toggleEnlarge()
+    
+    else if rndInt= createGame)500  , 1000)
+    rndInt.forEach(constrndInt => {
+        updateGame().toggle = ? 1, 0
+        p.demo.createLeft = toggleShrink());
+    return console.log(rndInt)
+}
+// transform: scale(1.3);
+// transform: scale(0.7);
