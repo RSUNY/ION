56e6eb19d22159497d3f0fe57830d407d7e804d3
Rishabh Suny <rishabhsunyriz@gmail.com>
2024-08-20 09:05:27 +0000

    known to git: addedfont styles to click me and color etc

--- JS-game/assets/js/script.js
+++ JS-game/assets/js/script.js
@@ -11,6 +11,7 @@ function  while (condition) {
 
 function updateGame() {}
 // the text will appear if the image enlarges or shrinks 
+// changed the text html displayed of click me as dmg is displayed
 
 
 function (element,forEach(img => {}
@@ -19,23 +20,45 @@ function (element,forEach(img => {}
 );
 
 var gameData = {
-    dmg: 1000
-    dmgPerClick: 5
+    dmg: 20
+    dmgPerClick: 1000 / 1000 + gameData.dmg.value
+    dmgDone1: 50
+    dmgDone: 20
 }
 
+var mainGameLoop = window.setInterval(function() {
+    updateGame()
+}, 1000)
 function updateGame() {
     gameData.dmg -= gameData.dmgPerClick
-    document.getElementById("dmgDone").innerHTML = gameData.dmg + "dmg"
+    document.getElementById("dmgDone").innerHTML = gameData.dmgPerClick + "dmg"
+    document.getElementById("dmgDone1").innerHTML = gameData.dmgPerClick + "dmg"
 }
 
-}
 
-function createGame(min , max) {
+
+
     
-    Math.floor(Math.random()* 5+1)
+     Math.floor(Math.random()* 5+1)
+     let indexes = myArray.push(index)
 
-   if (Math.floor(Math.random())* 5 !)* 10
-    indexof(createGame(0))
+     const myArray = [1, 2, 3, 4, 5];
+     const shuffledArray = shuffleArray(myArray);
+     console.log(shuffledArray);
+
+    const max = Math.max(...arr);
+    
+    const indexes = [];
+    
+    function createGame(min , max) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const randomIndex = Math.floor(Math.random() * (i + 1));
+            [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+        }
+        return array;
+    }
+    if (Math.floor(Math.random())* 5 !)* 10 => createGame(map(indexof(createGame()) , indexof(createGame(0))
+   ))
 
     else {}
     Math.floor(Math.random())* 5 !)* 10 > 10
